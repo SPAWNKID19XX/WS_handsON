5a92dd39048d1f17a26188bf771614dbc82e1d18
spawnkid19xx <borisisac@gmail.com>
2024-08-13 16:44:59 +0100

    [ADD] base.html with navbar and footer

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # Python 3.12.3
-from flask import Flask
+from flask import Flask, template_rendered, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    context = {
+        'title': 'Boris Isac',
+    }
+    return render_template('base.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
